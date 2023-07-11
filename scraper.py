e1122b1a8e00780fb2b82bf02beda3c365178788
Santiago Arias <santi99oca@gmail.com>
2023-07-11 19:33:38 -0300

    get exchange names

--- scraper.py
+++ scraper.py
@@ -10,8 +10,26 @@ XPATH_PRICES_SELL = '//span[@class="variation-max-min__value data-valor data-ven
 XPATH_NAMES_REFERENCE = '//span[@class="variation-max-min__description data-valor-descripcion" and contains(., "Referencia")]/ancestor::node()/div/h2[@class="variation-max-min__title"]/a/span/text()'
 XPATH_PRICES_REFERENCE = '//span[@class="variation-max-min__description data-valor-descripcion" and contains(., "Referencia")]/../span[1]/text()'
 
-def run():
+def parse_exchange():
+    try:
+        response = requests.get(HOME)
+        if response.status_code == 200:
+            home = response.content.decode('utf-8')
+            parsed = html.fromstring(home)
+            exchange_names = parsed.xpath(XPATH_NAMES_EXCHANGE)
+            print(exchange_names)
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+        
+    except ValueError as status:
+        print(status)
+
+def parse_reference():
     pass
+
+
+def run():
+    parse_exchange()
 
 if __name__ == "__main__":
     run()
